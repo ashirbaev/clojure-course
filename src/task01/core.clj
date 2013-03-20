7de4ca6283696745c63888a108995fc66e2eda83
Ildar-Ashirbaev <ildar.ashirbaev@gmail.com>
2013-03-21 02:44:46 +0400

    Update core.clj

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -3,6 +3,35 @@
   (:gen-class))
 
 
+(defn get-href [tag]
+  (map #(:href (second %))
+    (filter #(= (first %) :a) (rest(rest tag)))))
+
+
+(defn get-links []
+  (let [data (parse "clojure_google.html")]
+    (loop [result [], tags (list data)]
+      (if (empty? tags)
+        result
+        (let [tag (first tags)]
+          (recur (if (= (second tag) {:class "r"})
+                   (concat result (get-href tag))
+                   result)
+            (concat (next tags) (filter #(keyword? (first %)) (rest(rest tag))))))))))
+
+
+(defn -main []
+  (let [result (get-links)]
+    (do
+;      (println result)
+      (println (str "Found " (count result) " links!")))))
+
+
+(ns task01.core
+  (:require [pl.danieljanus.tagsoup :refer :all])
+  (:gen-class))
+
+
 (defn get-links []
 " 1) Find all elements containing {:class \"r\"}.
 
