26f09dfee41043a89a305dc0329a435cbc770bf5
ildar-ashirbaev <ildar.ashirbaev@gmail.com>
2013-03-28 02:10:29 +0400

    Single threaded file searcher

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,13 +1,37 @@
 (ns clojure-course-task02.core
+  (:import (java.io File))
   (:gen-class))
+
+
+(def result (ref []))
+
+
+(defn walker [matcher-func next-data-func data]
+  (let [result (matcher-func data)
+        next-data (next-data-func data)]
+    (if-not (empty? next-data)
+      (conj result
+            (doall
+              (map #(walker matcher-func next-data-func %) next-data)))
+      result)))
+
+
+(defn matcher [regexp]
+  (fn [path]
+    (if (and (not (.isDirectory path)) (.matches (.getName path) regexp))
+      (.getName path)
+      nil)))
 
 
 (defn find-files [file-name path]
   "TODO: Implement searching for a file using his name as a regexp."
-  nil)
+  (vec (filter #(not (nil? %))
+       (flatten (walker (matcher file-name) #(vec (.listFiles %)) (new File path))))))
+
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
+
 
 (defn -main [file-name path]
   (if (or (nil? file-name)
